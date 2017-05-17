dff655309a845f1b27d36feb48f71f50ed8aeb61
jasonshao19960516 <jasonshao19960516@gmail.com>
2017-05-17 00:14:42 -0400

    day2

--- index.js
+++ index.js
@@ -1,37 +1,15 @@
 const personForm = document.querySelector('form');
 
-const renderColor = (hairColor) => {
-  const colorDiv = document.createElement('div');
-  colorDiv.style.backgroundColor = hairColor;
-  colorDiv.style.height = '50px';
-  colorDiv.style.width = '100px';
-  return colorDiv;
-}
-
 const handleSubmit = (ev) => {
   ev.preventDefault()
   const form = ev.target
-  const details = document.querySelector('.details')
+  const list = document.querySelector('.rosterList')
 
  const personName = form.personName.value
-  const name = document.createElement("p")
+  const name = document.createElement('li')
   name.textContent = personName
-
-  const hairColor = form.hairColor.value
-   const colorDiv = renderColor(hairColor)
-
-  const age = form.age.value
-   const ageX = document.createElement("p")
-  ageX.textContent = age
-
-  const birthplace = form.birthplace.value
-  const birth = document.createElement("p")
-  birth.textContent = birthplace
   
-  details.appendChild(name)
-  details.appendChild(colorDiv)
-  details.appendChild(ageX)
-  details.appendChild(birth)
+  list.insertBefore(name, list.firstChild)
 
 }
 
